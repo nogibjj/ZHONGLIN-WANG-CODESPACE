0e4c42bd0a73b141d13e13a239cc5e3a95441b49
zw277 <112585430+zw277@users.noreply.github.com>
2022-09-17 04:46:14 +0000

    update

--- dblib/querydb.py
+++ dblib/querydb.py
@@ -2,7 +2,7 @@ from databricks import sql
 import os
 
 
-def querydb(query="SELECT * FROM default.diamonds LIMIT 2"):
+def querydb(query="SELECT * FROM ethtransactions.csv LIMIT 15"):
     with sql.connect(
         server_hostname=os.getenv("DATABRICKS_SERVER_HOSTNAME"),
         http_path=os.getenv("DATABRICKS_HTTP_PATH"),
