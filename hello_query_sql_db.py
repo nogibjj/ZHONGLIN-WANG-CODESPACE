2fed489e4b1058c6f473c00610ede5df97ba73ba
zw277 <112585430+zw277@users.noreply.github.com>
2022-09-18 01:32:54 +0000

    update

--- hello_query_sql_db.py
+++ hello_query_sql_db.py
@@ -3,24 +3,24 @@
 import click
 from dblib.querydb import querydb
 
-# build a click group
+#build a click group
 @click.group()
 def cli():
-    """A simple CLI to query a SQL database"""
-
+    """A simple CLI to query a SQL from eth trading database to search the latest transactions information"""
 
 # build a click command
 @cli.command()
 @click.option(
     "--query",
-    default="SELECT * FROM default.diamonds LIMIT 2",
+    default="SELECT * FROM eth LIMIT 15",
     help="SQL query to execute",
 )
 def cli_query(query):
-    """Execute a SQL query"""
+    """Execute a SQL query from eth trading database to search the latest transactions information"""
     querydb(query)
 
 
-# run the CLI
+  
+  #run the CLI
 if __name__ == "__main__":
     cli()
